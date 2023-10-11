c0d0085a1362ce12e7257b86b635fab7b41dfbe7
nagmo01 <bjworkshop01@gmail.com>
2023-10-11 12:34:04 +0900

    if文を簡潔にしました。これで１回目PRだします

--- calendar.rb
+++ calendar.rb
@@ -7,71 +7,60 @@ opt.parse!(ARGV)
 
 #current_dateに今年、今月、今日を代入
 current_date = Date.today
-#inputにコマンドライン引数を代入
-input = ARGV[0]
-
-#受け取った引数が整数値判別する
-if input != nil
-  #引数の文字列が整数か確認(少数も省く）
-  if input =~ /\A-?\d+\z/
-    #引数が1-12に収まっているか確認
-    if input.to_i >= 1 && input.to_i <= 12
-      year = current_date.year
-      puts "      #{input}月 #{year}"
-      month = input.to_i
-    else
-      puts "#{input} is neither a month number (1..12) nor a name"
-      exit
-
-    end
-
-  else
-    puts "#{input} is neither a month number (1..12) nor a name"
-    exit
-  end
-
-#引数なしの場合当月を表示する
+#引数有りで実行された場合そのままinputに代入
+#引数無しで実行された場合今月をinputに代入（文字列に直す）
+if ARGV[0] != nil
+  input = ARGV[0]
 else
+  input = current_date.month.to_s
+end
+
+#input（引数）が1~12の範囲内であるかどうかを確認する（引数無しは1~12に絶対含まれる）
+#includeで確認するときに整数で確認するとto_sを使うことになり、引数が少数だった場合に整数にしてしまうので文字列のままincludeで確認します。
+array = (1..12).map(&:to_s)
+if array.include?(input)
   year = current_date.year
-  month = current_date.month
-  puts "      #{month}月 #{year}"
-end
-
-
-
-
-
-#これで当月の初日が何曜日かわかる 0=日曜日
-wday = Date.new(current_date.year, month, 1).wday
-#これで最終日が何日かを取得
-last_day = Date.new(current_date.year, month, -1).day
-#この条件分岐で日曜始まりを月曜始まりに変える
-if wday == 0
-  monday_wday = 6
+  month = input.to_i
 else
-  monday_wday = wday - 1
+  puts "#{input} is neither a month number (1..12) nor a name"
+  exit
 end
 
-# iはループの判定に利用。実際に日数の出力時に+1加算して利用する
-i = 1 
-# dは1~7までを一区切りとして改行の管理と1日の曜日まで空白を入れるために使います。# dはループの回数で+1加算
-d = 1 
-print "月 火 水 木 金 土 日\n"
+#日曜始まりでの月初日を取得
+month_start = Date.new(year, month, 1).wday
+
+#日曜始まりを月曜始まりに直す。
+month_start = (month_start == 0) ? 6 : month_start - 1
+
+#月末日を取得
+last_day = Date.new(year, month, -1).day
+
+#カレンダー表示の処理
+
+puts "      #{input}月 #{year}"
+
+puts "月 火 水 木 金 土 日\n"
+
+#iは実際にカレンダーの日数を出力したときに加算していくカウンタ
+#dはループ回数自体で加算していくカウンタにしていて、月〜日の７回ごとに改行を挟むために利用します。
+i = 1
+d = 1
+
 while i <= last_day
 
-
-  if d <= monday_wday
+  if d <= month_start
     print "   "
+
   else
     printf("%2d", i)
     print " "
     i += 1
   end
 
-
-
   if d % 7 == 0
     print "\n"
   end
+
   d += 1
+
 end
